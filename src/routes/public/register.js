97b965e8716f7725018474c2e03d5d8918deffec
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-14 00:27:24 -0300

    feat: sending user to database

--- src/routes/public/register.js
+++ src/routes/public/register.js
@@ -1,23 +1,30 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client'
+
 
 const router = express.Router()
+const prisma = new PrismaClient()
 
+//Cadastro
 router.post('/register', async (req, res) => {
   try {
-    const users = [] //simulando o bando de dados por enquanto
+    const user = req.body
 
     const salt = await bcrypt.genSalt(10)
     const hashPassword = await bcrypt.hash(req.body.password, salt)
 
-    users.push({
-      name: req.body.name,
-      email: req.body.email,
-      password: hashPassword
+    const userDB = await prisma.users.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: hashPassword
+      }
     })
 
-    res.status(201).json(users)
+    res.status(201).json(userDB)
   } catch (error) {
+    console.log(error)
     res.status(500).json({ message: 'Erro no servidor, tente novamente' })
   }
 })
