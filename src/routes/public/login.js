43e13f4f36d9d6e86801e4cd493ed7ad7ad843fa
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-14 01:03:50 -0300

    feat: implementing JWT authentication token

--- src/routes/public/login.js
+++ src/routes/public/login.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import { PrismaClient } from '@prisma/client'
 
 
 const router = express.Router()
 const prisma = new PrismaClient()
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 //Login
 router.post('/login', async (req, res) => {
@@ -21,13 +24,16 @@ router.post('/login', async (req, res) => {
       return res.status(404).json({ message: 'Usuário não encontrado no sistema' })
     }
 
+    //compara a senha armazenada no banco com a que o usuário digitou
     const isMatch = await bcrypt.compare(userInfo.password, user.password)
 
     if (!isMatch) {
       return res.status(400).json({ message: 'Senha inválida' })
     }
 
-    res.status(200).json(user)
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1m' }) //criando token de autenticação
+
+    res.status(200).json(token)
   } catch (error) {
     res.status(500).json({ message: 'Erro no servidor, tente novamente' })
   }
