f95361e21ccaec85cfaaef466eefc6467ec83e8d
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-14 00:49:44 -0300

    chore: rename variable

--- src/routes/public/register.js
+++ src/routes/public/register.js
@@ -9,15 +9,15 @@ const prisma = new PrismaClient()
 //Cadastro
 router.post('/register', async (req, res) => {
   try {
-    const user = req.body
+    const userInfo = req.body
 
     const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(req.body.password, salt)
+    const hashPassword = await bcrypt.hash(userInfo.password, salt)
 
     const userDB = await prisma.users.create({
       data: {
-        name: user.name,
-        email: user.email,
+        name: userInfo.name,
+        email: userInfo.email,
         password: hashPassword
       }
     })
